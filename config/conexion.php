92aba39b56d0745ec3af5012d26ef7095e0e4378
Antonio Rivas <98570835+261098@users.noreply.github.com>
2022-07-26 02:19:59 -0600

    Primer commit del proyecto, conexion

---
+++ config/conexion.php
@@ -0,0 +1,21 @@
+<?php
+    class Conectar{
+      protected $dbh;
+
+   protected function Conexion(){
+     try {
+      $conectar = $this->dbh = new PDO("mysql:host=20.216.41.245;dbname=g8_19", "G8_19", "Pn4tiUyt");
+      return $conectar; 
+    } 
+      catch (Exception $e) {
+      print "¡Error BD!: " . $e->getMessage() . "<br/>"; 
+      die();
+    }
+  }
+   public function set_names(){
+  return $this->dbh->query("SET NAMES 'utf8'");
+  }
+
+  }
+
+?>
